b9badd2dad6f6122729c67b2b40f4d8d756ce4d8
Dwayne Crooks <me@dwaynecrooks.com>
2024-04-25 05:37:52 -0400

    LETREC: Start with PROC

---
+++ src/Ch3/LETREC/AST.elm
@@ -0,0 +1,29 @@
+module Ch3.LETREC.AST exposing
+    ( Expr(..)
+    , Id
+    , Number
+    , Program(..)
+    )
+
+
+type Program
+    = Program Expr
+
+
+type Expr
+    = Const Number
+    | Var Id
+    | Diff Expr Expr
+    | Zero Expr
+    | If Expr Expr Expr
+    | Let Id Expr Expr
+    | Proc Id Expr
+    | Call Expr Expr
+
+
+type alias Number =
+    Int
+
+
+type alias Id =
+    String
---
+++ src/Ch3/LETREC/Parser.elm
@@ -0,0 +1,131 @@
+module Ch3.LETREC.Parser exposing (Error, parse)
+
+import Ch3.LETREC.AST as AST exposing (..)
+import Lib.Lexer as L
+import Parser as P exposing ((|.), (|=), Parser)
+import Set
+
+
+type alias Error =
+    List P.DeadEnd
+
+
+parse : String -> Result (List P.DeadEnd) AST.Program
+parse =
+    P.run program
+
+
+program : Parser AST.Program
+program =
+    P.succeed Program
+        |. L.spaces
+        |= expr
+        |. P.end
+
+
+expr : Parser Expr
+expr =
+    P.oneOf
+        [ diffExpr
+        , zeroExpr
+        , ifExpr
+        , letExpr
+        , procExpr
+        , callExpr
+        , constExpr
+        , varExpr
+        ]
+
+
+diffExpr : Parser Expr
+diffExpr =
+    P.succeed Diff
+        |. L.symbol "-"
+        |. L.symbol "("
+        |= P.lazy (\_ -> expr)
+        |. L.symbol ","
+        |= P.lazy (\_ -> expr)
+        |. L.symbol ")"
+
+
+zeroExpr : Parser Expr
+zeroExpr =
+    P.succeed Zero
+        |. L.keyword "zero?"
+        |. L.symbol "("
+        |= P.lazy (\_ -> expr)
+        |. L.symbol ")"
+
+
+ifExpr : Parser Expr
+ifExpr =
+    P.succeed If
+        |. L.keyword "if"
+        |= P.lazy (\_ -> expr)
+        |. L.keyword "then"
+        |= P.lazy (\_ -> expr)
+        |. L.keyword "else"
+        |= P.lazy (\_ -> expr)
+
+
+letExpr : Parser Expr
+letExpr =
+    P.succeed Let
+        |. L.keyword "let"
+        |= id
+        |. L.symbol "="
+        |= P.lazy (\_ -> expr)
+        |. L.keyword "in"
+        |= P.lazy (\_ -> expr)
+
+
+procExpr : Parser Expr
+procExpr =
+    P.succeed Proc
+        |. L.keyword "proc"
+        |. L.symbol "("
+        |= id
+        |. L.symbol ")"
+        |= P.lazy (\_ -> expr)
+
+
+callExpr : Parser Expr
+callExpr =
+    P.succeed Call
+        |. L.symbol "("
+        |= P.lazy (\_ -> expr)
+        |= P.lazy (\_ -> expr)
+        |. L.symbol ")"
+
+
+constExpr : Parser Expr
+constExpr =
+    P.map Const number
+
+
+number : Parser Number
+number =
+    L.digits
+
+
+varExpr : Parser Expr
+varExpr =
+    P.map Var id
+
+
+id : Parser String
+id =
+    L.makeIdentifier
+        { start = Char.isLower
+        , inner = Char.isLower
+        , reserved =
+            Set.fromList
+                [ "else"
+                , "if"
+                , "in"
+                , "let"
+                , "proc"
+                , "then"
+                , "zero?"
+                ]
+        }
