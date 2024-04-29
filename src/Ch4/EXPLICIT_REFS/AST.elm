741780b3b6c6a3a8e55b7b3b27285b43c5f66f07
Dwayne Crooks <me@dwaynecrooks.com>
2024-04-29 06:15:47 -0400

    EXPLICIT-REFS: Start with LETREC

---
+++ src/Ch4/EXPLICIT_REFS/AST.elm
@@ -0,0 +1,30 @@
+module Ch4.EXPLICIT_REFS.AST exposing
+    ( Expr(..)
+    , Id
+    , Number
+    , Program(..)
+    )
+
+
+type Program
+    = Program Expr
+
+
+type Expr
+    = Const Number
+    | Var Id
+    | Diff Expr Expr
+    | Zero Expr
+    | If Expr Expr Expr
+    | Let Id Expr Expr
+    | Proc Id Expr
+    | Letrec Id Id Expr Expr
+    | Call Expr Expr
+
+
+type alias Number =
+    Int
+
+
+type alias Id =
+    String
---
+++ src/Ch4/EXPLICIT_REFS/Env.elm
@@ -0,0 +1,61 @@
+module Ch4.EXPLICIT_REFS.Env exposing
+    ( Env
+    , Item(..)
+    , empty
+    , extend
+    , extendRec
+    , find
+    )
+
+
+type
+    Env id v e
+    --  ^  ^ ^
+    --  |  | |
+    --  |  | +--- the type for expressions
+    --  |  +----- the type for values
+    --  +-------- the type for identifiers
+    = Empty
+    | Bind id v (Env id v e) -- name value
+    | RecBind id id e (Env id v e) -- name param body
+
+
+empty : Env id v e
+empty =
+    Empty
+
+
+extend : id -> v -> Env id v e -> Env id v e
+extend =
+    Bind
+
+
+extendRec : id -> id -> e -> Env id v e -> Env id v e
+extendRec =
+    RecBind
+
+
+type Item id v e
+    = Value v
+    | Procedure id e (Env id v e)
+
+
+find : id -> Env id v e -> Maybe (Item id v e)
+find needle env =
+    case env of
+        Empty ->
+            Nothing
+
+        Bind name value restEnv ->
+            if needle == name then
+                Just <| Value value
+
+            else
+                find needle restEnv
+
+        RecBind name param body restEnv ->
+            if needle == name then
+                Just <| Procedure param body env
+
+            else
+                find needle restEnv
